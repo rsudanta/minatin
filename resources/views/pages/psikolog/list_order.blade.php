ab5ed914d7d1bb547c4dac7fec35a8f4d04a6d56
dela <delanika20@gmail.com>
2020-12-08 20:58:42 +0700

    akhirnya bener bgsd

--- resources/views/pages/psikolog/list_order.blade.php
+++ resources/views/pages/psikolog/list_order.blade.php
@@ -13,9 +13,9 @@
     <!--End Header-->
     <div class="content-order">
         <h1 class="h1-content">MENUNGGU PENILAIAN</h1>
-        <div class="row  mb-4">
-            <div class="col-4 mr-4">
-                @forelse ($finish as $item)
+        <div class="row mb-4">
+            @forelse ($finish as $item)
+            <div class="col-4 mb-4">
                 <a href="{{ route('psikolog.hasil', $item->id) }}">
                     <div class="card-pemesan text-center ">
                         <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
@@ -27,24 +27,24 @@
                 </a>
             </div>
             @empty
-            <p>Belum ada order</p>
+            <p style="margin-left: 15px">Belum ada order</p>
             @endforelse
         </div>
-        <div class="content-paid">
-            <h1 class="h1-content">BELUM DIKERJAKAN</h1>
-            <div class="row">
-                <div class="col-4 mr-4">
-                    @forelse ($paid as $item)
-                    <div class="card-pemesan-paid text-center ">
-                        <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
-                        <div class="nama-pemesan">{{ $item->user->name }}</div>
-                        <div class="order-id">Order ID #{{ $item->id }}</div>
-                    </div>
+    </div>
+    <div class="content-paid">
+        <h1 class="h1-content">BELUM DIKERJAKAN</h1>
+        <div class="row">
+            @forelse ($paid as $item)
+            <div class="col-4 mb-4">
+                <div class="card-pemesan-paid text-center ">
+                    <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
+                    <div class="nama-pemesan">{{ $item->user->name }}</div>
+                    <div class="order-id">Order ID #{{ $item->id }}</div>
                 </div>
-                @empty
-                <p>Belum ada order</p>
-                @endforelse
             </div>
+            @empty
+            <p style="margin-left: 15px">Belum ada order</p>
+            @endforelse
         </div>
     </div>
 </div>
