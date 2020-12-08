b7df2ac308126c211a046807d0c87b8505347503
dela <delanika20@gmail.com>
2020-12-08 18:41:23 +0700

    benerin fe list order

--- resources/views/pages/psikolog/list_order.blade.php
+++ resources/views/pages/psikolog/list_order.blade.php
@@ -23,15 +23,15 @@
                 <div class="col-4 mr-4">
                     @forelse ($finish as $item)
                         <a href="{{ route('psikolog.hasil', $item->user->id) }}">
-                            <div class="card-pemesan text-center ">
+                            <div class="card-pemesan text-center">
                                 <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
                                 <div class="nama-pemesan">{{ $item->user->name }}</div>
                                 <div class="order-id">Order ID #{{ $item->id }}</div>
                                 <hr>
-                                <button class="status-nilai belum" disabled>Beri peniliaan</button>
+                                <button class="status-nilai belum" disabled>Beri penilaian</button>
                             </div>
                         </a>
-                    </div>
+                </div>
                 @empty
                     <p>Belum ada order</p>
                 @endforelse
@@ -42,14 +42,12 @@
             <div class="row">
                 <div class="col-4 mr-4">
                     @forelse ($paid as $item)
-                        <div class="card-pemesan text-center ">
+                        <div class="card-pemesan-paid text-center ">
                             <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
                             <div class="nama-pemesan">{{ $item->user->name }}</div>
                             <div class="order-id">Order ID #{{ $item->id }}</div>
-                            <hr>
-                            <button class="status-nilai sudah" disabled>Beri peniliaan</button>
                         </div>
-                    </div>
+                </div>
                 @empty
                     <p>Belum ada order</p>
                 @endforelse
