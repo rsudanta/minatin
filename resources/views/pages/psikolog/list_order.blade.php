065c3835de40f582e14ba757a01bee1a878d4d7d
dela <delanika20@gmail.com>
2020-12-08 20:13:51 +0700

    benerin margin dikit

--- resources/views/pages/psikolog/list_order.blade.php
+++ resources/views/pages/psikolog/list_order.blade.php
@@ -1,57 +1,51 @@
-@extends('layouts.psikolog.app_psikolog')
-
-@section('title')
-    Order
-@endsection
-
-@section('content')
-    <div class="main">
-        <!--Header-->
-        <div class="fixed-top judul-page">
-            <h1 class="title">
-                Order
-            </h1>
-            <p> <a href="#" class="bread-nav">
+@extends('layouts.psikolog.app_psikolog') @section('title') Order @endsection @section('content')
+<div class="main">
+    <!--Header-->
+    <div class="fixed-top judul-page">
+        <h1 class="title">
+            Order
+        </h1>
+        <p> <a href="#" class="bread-nav">
                     Order
                 </a>
-            </p>
-        </div>
-        <!--End Header-->
-        <div class="content-order">
-            <h1 class="h1-content">MENUNGGU PENILAIAN</h1>
-            <div class="row">
-                <div class="col-4 mr-4">
-                    @forelse ($finish as $item)
-                        <a href="{{ route('psikolog.hasil', $item->id) }}">
-                            <div class="card-pemesan text-center ">
-                                <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
-                                <div class="nama-pemesan">{{ $item->user->name }}</div>
-                                <div class="order-id">Order ID #{{ $item->id }}</div>
-                                <hr>
-                                <button class="status-nilai belum" disabled>Beri penilaian</button>
-                            </div>
-                        </a>
-                </div>
-                @empty
-                    <p>Belum ada order</p>
-                @endforelse
+        </p>
+    </div>
+    <!--End Header-->
+    <div class="content-order">
+        <h1 class="h1-content">MENUNGGU PENILAIAN</h1>
+        <div class="row  mb-4">
+            <div class="col-4 mr-4">
+                @forelse ($finish as $item)
+                <a href="{{ route('psikolog.hasil', $item->id) }}">
+                    <div class="card-pemesan text-center ">
+                        <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
+                        <div class="nama-pemesan">{{ $item->user->name }}</div>
+                        <div class="order-id">Order ID #{{ $item->id }}</div>
+                        <hr>
+                        <button class="status-nilai belum" disabled>Beri penilaian</button>
+                    </div>
+                </a>
             </div>
-        </div>
-        <div class="content-paid">
-            <h1 class="h1-content">BELUM DIKERJAKAN</h1>
-            <div class="row">
-                <div class="col-4 mr-4">
-                    @forelse ($paid as $item)
-                        <div class="card-pemesan-paid text-center ">
-                            <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
-                            <div class="nama-pemesan">{{ $item->user->name }}</div>
-                            <div class="order-id">Order ID #{{ $item->id }}</div>
-                        </div>
-                </div>
-                @empty
-                    <p>Belum ada order</p>
-                @endforelse
-            </div>
+            @empty
+            <p>Belum ada order</p>
+            @endforelse
         </div>
     </div>
+    <div class="mb-4">
+        <h1 class="h1-content">BELUM DIKERJAKAN</h1>
+        <div class="row">
+            <div class="col-4 mr-4">
+                @forelse ($paid as $item)
+                <div class="card-pemesan-paid text-center ">
+                    <img src="/storage/avatars/{{ $item->user->avatar }}" alt="" class="rounded-circle img-pemesan">
+                    <div class="nama-pemesan">{{ $item->user->name }}</div>
+                    <div class="order-id">Order ID #{{ $item->id }}</div>
+                </div>
+            </div>
+            @empty
+            <p>Belum ada order</p>
+            @endforelse
+        </div>
+    </div>
+</div>
 @endsection
