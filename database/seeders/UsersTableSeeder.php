805cfbe1ef17d7a14e1407827d3ad6e8521b6c52
dela <delanika20@gmail.com>
2020-12-09 09:47:07 +0700

    seed psikolog

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -26,5 +26,16 @@ class UsersTableSeeder extends Seeder
             'password' => bcrypt('password'),
             'roles' => 'USER'
         ]);
+        DB::table('users')->insert([
+            'name' => 'psikolog',
+            'email' => 'psikolog@psikolog.com',
+            'password' => bcrypt('password'),
+            'roles' => 'PSIKOLOG',
+            'penyelenggara' => 'Healthy Life',
+            'bidang' => 'Psikolog Anak',
+            'lama_kerja' => '4',
+            'edukasi' => 'S3 Psikolog Universitas Padjadjaran',
+            'deskripsi' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor tellus nunc, id sodales nunc venenatis non. Integer semper eget dolor sed malesuada. Ut finibus enim in ligula dictum cursus. Ut semper neque sed nunc consequat accumsan. Donec vehicula eros elit, ac suscipit tortor bibendum ut. Mauris tortor nisi, convallis a dui eget, molestie venenatis mauris. Pellentesque tincidunt, sapien eget vehicula aliquam, tellus lectus faucibus massa, ut auctor ante eros sed mi. Aenean ornare orci congue risus tristique, sed imperdiet risus consequat. Nam volutpat nisi vel purus fermentum sodales.',
+        ]);
     }
 }
